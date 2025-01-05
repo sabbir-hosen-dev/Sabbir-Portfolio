a00fb718c010d67d0f14c9f1cf2edb4f74165ff2
sabbir-hosen-dev <tssabbirhosen@gmail.com>
2025-01-05 11:27:05 +0600

    name update

--- src/Components/CodeBox/CodeBox.jsx
+++ src/Components/CodeBox/CodeBox.jsx
@@ -29,7 +29,7 @@ function CoadBox() {
           <div>
             <span className="ml-4 lg:ml-8 mr-2 text-white">name:</span>
             <span className="text-gray-400">'</span>
-            <span className="text-amber-300">Abu Said</span>
+            <span className="text-amber-300">Sabbir Hosen</span>
             <span className="text-gray-400">',</span>
           </div>
           <div className="ml-4 lg:ml-8 mr-2">
