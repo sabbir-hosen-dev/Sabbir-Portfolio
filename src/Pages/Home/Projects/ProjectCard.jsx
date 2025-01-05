4d898ea6db87e77c73e64d7aea284ea07924b3ac
sabbir-hosen-dev <tssabbirhosen@gmail.com>
2025-01-05 15:10:24 +0600

    fixt

--- src/Pages/Home/Projects/ProjectCard.jsx
+++ src/Pages/Home/Projects/ProjectCard.jsx
@@ -25,16 +25,16 @@ const ProjectCard = ({ project }) => {
           <h3 className="text-2xl font-semibold text-white">{name}</h3>
           <p>{truncatedDescription}</p>
         </div>
-        <div className="flex flex-wrap gap-5">
+        <div className="flex flex-wrap gap-3">
           {tools.map(tool => (
-            <div key={tool.id} className="flex flex-wrap gap-2">
+            <div key={tool.id} className="flex flex-wrap">
               <div className="inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 bg-gray-700/50 hover:bg-gray-700 text-gray-100 border-gray-600">
                 {tool}
               </div>
             </div>
           ))}
         </div>
-        <div className="flex justify-between mt-4">
+        <div className="flex justify-between items-center mt-4">
           <Link className="text-emerald-400 hover:opacity-50 transition-all duration-300 text-sm font-mono mb-2 tracking-wide uppercase">
             View More Details
           </Link>
@@ -44,41 +44,14 @@ const ProjectCard = ({ project }) => {
               className="text-slate-400 hover:text-emerald-400 transition-all duration-300 transform hover:scale-125"
               target="_blank"
               rel="noopener noreferrer">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="22"
-                height="22"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className="lucide lucide-github">
-                <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"></path>
-                <path d="M9 18c-4.51 2-5-2-7-2"></path>
-              </svg>
+                <RiGithubLine className='text-xl' />
             </a>
             <a
               href={liveLink}
               className="text-slate-400 hover:text-emerald-400 transition-all duration-300 transform hover:scale-125"
               target="_blank"
               rel="noopener noreferrer">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="22"
-                height="22"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className="lucide lucide-external-link">
-                <path d="M15 3h6v6"></path>
-                <path d="M10 14 21 3"></path>
-                <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
-              </svg>
+                <BiLinkExternal className='text-xl' />
             </a>
           </div>
         </div>
