c9eff02e3a577c47950d79806ddd01cdc8634bea
sabbir-hosen-dev <tssabbirhosen@gmail.com>
2025-02-04 03:38:09 +0600

    resume pdg link

--- src/Components/Navber/Navber/Navber.jsx
+++ src/Components/Navber/Navber/Navber.jsx
@@ -83,7 +83,7 @@ function Navbar({ openMenu, setMenu }) {
                 <a
                   className="block lg:hidden"
                   target='_blank'
-                  href="https://drive.google.com/file/d/1HeWJ0vNGGUPAl6iU03JJs6qIApBimSy9/view?usp=drive_link">
+                  href="https://drive.google.com/drive/folders/1urwOHhANiAFSddmn1gJhgCn_QNp8fxUX">
                   <Btn text="Resume" />
                 </a>
               </ul>
@@ -113,7 +113,7 @@ function Navbar({ openMenu, setMenu }) {
                   <BiSun className="links" />
                 )}
               </div> */}
-              <a target='_blank'  href="https://drive.google.com/file/d/1HeWJ0vNGGUPAl6iU03JJs6qIApBimSy9/view?usp=drive_link">
+              <a target='_blank'  href="https://drive.google.com/drive/folders/1urwOHhANiAFSddmn1gJhgCn_QNp8fxUX">
               <Btn text="Resume" /></a>
              
             </div>
--- src/Pages/Home/Banner/BannerLeft.jsx
+++ src/Pages/Home/Banner/BannerLeft.jsx
@@ -101,7 +101,7 @@ function BannerLeft() {
             role="button"
             target="_blank"
             rel="noopener noreferrer"
-            href="https://drive.google.com/file/d/1HeWJ0vNGGUPAl6iU03JJs6qIApBimSy9/view?usp=drive_link"
+            href="https://drive.google.com/drive/folders/1urwOHhANiAFSddmn1gJhgCn_QNp8fxUX"
             whileHover={{ scale: 1.05 }}
           >
             <span>Get Resume</span>
